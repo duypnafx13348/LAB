975b183567a61b2eda4ed8b62c349e58faf5e3f9
DuypnaFX13348 <duypna0604@gmail.com>
2021-12-30 21:43:55 +0700

    Function Component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 import dateFormat from 'dateformat';
 
-class DishDetail extends Component {
-
-    renderDish(dish) {
+    function RenderDish({dish}) {
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
@@ -18,7 +16,7 @@ class DishDetail extends Component {
         );
     }
 
-    renderComments(comments) {
+    function RenderComments(comments) {
         if (comments != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -42,13 +40,13 @@ class DishDetail extends Component {
         }
     }
 
-    render() {
-        if (this.props.dish != null) {
+    const DishDetail= (props) => {
+        if (props.dish != null) {
             return(
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
             );
@@ -58,6 +56,5 @@ class DishDetail extends Component {
             );
         }
     }
-}
 
     export default DishDetail;
